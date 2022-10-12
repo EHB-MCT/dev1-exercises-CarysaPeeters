7619ca6a141f6426e7a490dae33d663c0972926a
Carysa Peeters <carysa.peeters@student.ehb.be>
2022-10-12 16:28:54 +0200

    random colours done

--- exercises/2-variables/2-2-random colours.js
+++ exercises/2-variables/2-2-random colours.js
@@ -5,33 +5,22 @@ canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 let context = canvas.getContext('2d');
 
-let width = context.canvas.width;
-let height = context.canvas.height;
-let r;
-let g;
-let b;
-let color;
+drawSquares(50,50,400);
+drawSquares(75,75,350);
+drawSquares(100,100,300);
+drawSquares(125,125,250);
+drawSquares(150,150,200);
+drawSquares(175,175,150);
+drawSquares(200,200,100);
+drawSquares(225,225,50);
 
-
-
-
-drawSquares(50,50,400,400);
-drawSquares(75,75,350,350);
-drawSquares(100,100,300,300);
-drawSquares(125,125,250,250);
-drawSquares(150,150,200,200);
-drawSquares(175,175,150,150);
-drawSquares(200,200,100,100);
-drawSquares(225,225,50,50);
-
-
-function drawSquares(x, y, width, height) {
+function drawSquares(x, y, size) {
     let r = Math.floor(Math.random()*255);
     let g = Math.floor(Math.random()*255);
     let b = Math.floor(Math.random()*255);
     let color = "rgb("+ r + ","+ g + ","+ b +")";
     context.fillStyle = color;
-    context.fillRect(x,y,width,height);
+    context.fillRect(x,y,size,size);
 }
 
 console.log(r,g,b);
