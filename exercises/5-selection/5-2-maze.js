ceec35dd6e40c5c3bd21d960c23647f2d55efdb8
Carysa Peeters <carysa.peeters@student.ehb.be>
2022-11-09 16:25:42 +0100

    5-2 done

---
+++ exercises/5-selection/5-2-maze.js
@@ -0,0 +1,30 @@
+"use strict";
+import context from "../../../scripts/context.js";
+import * as Utils from "../../../scripts/utils.js";
+
+let width = context.canvas.width;
+let height = context.canvas.height;
+
+context.fillStyle = "black";
+context.fillRect (0, 0, width, height);
+
+drawMaze ();
+
+function drawMaze () {
+    context.lineWidth = 5;
+    context.lineCap = "square";
+    
+    for (let j = 0; j < 200; j++) {
+        for (let i = 0; i < 200; i++) {
+            context.strokeStyle = Utils.hsl((Math.random()*360),100, 50);
+            let r = Math.round(Math.random());
+            if (r == 0) {
+                Utils.drawLine(i*10, j*10+10, i*10+10, j*10);
+            } else {
+                Utils.drawLine(i*10, j*10, i*10+10, j*10+10);
+            } 
+        } 
+    } 
+}
+
+
