877c8176ef44d5a5b1bb376dc8d8a21011884a15
Carysa Peeters <carysa.peeters@student.ehb.be>
2022-11-24 18:25:03 +0100

    6-2 done

--- exercises/6-animation/6-2-bouncing-bubble.js
+++ exercises/6-animation/6-2-bouncing-bubble.js
@@ -5,20 +5,59 @@ import * as Utils from "../../scripts/utils.js";
 let width = context.canvas.width;
 let height = context.canvas.height;
 
-let frameCount = 0;
-let xPos = width / 2;
-let yPos = height/2;
-context.fillRect (0, 0, width, height);
+let x;
+let y;
+let xSpeed = 10;
+let ySpeed = 10;
+let radius = 100;
 
+let isPlaying = true;
 
-update();
-function update () {
-    frameCount++;
-    context.fillStyle = "black";
-    context.fillStyle = Utils.hsl(frameCount, 100, 50);
-    Utils.fillcircle(xPos, yPos, 25);
-    xPos += Utils.randomNumber (-10, 5);
-    yPos += Utils.randomNumber (-5, 5);
-    requestAnimationFrame(update);
+window.onclick = click;
+
+setup();
+
+function setup() {
+	x = width / 2;
+	y = height / 2;
+
+	update();
 }
 
+function update() {
+	if (isPlaying) {
+		//clear canvas
+		context.fillStyle = "white";
+		context.fillRect(0, 0, width, height);
+
+		//updating x and y coordinate
+		x += xSpeed;
+		y += ySpeed;
+
+		//check x and y border
+		if (x >= width - radius || x <= radius) {
+			xSpeed *= -1;
+		}
+
+		if (y >= height - radius || y <= radius) {
+			ySpeed *= -1;
+		}
+
+		//draw a circle
+		context.fillStyle = "blue";
+		Utils.fillCircle(x, y, radius);
+
+		requestAnimationFrame(update);
+	}
+}
+
+function click(eventData) {
+	let mouseX = eventData.x;
+	let mouseY = eventData.y;
+
+	if (Utils.calculateDistance(x, y, mouseX, mouseY) < radius) {
+		isPlaying = false;
+		context.fillStyle = "red";
+		Utils.fillCircle(mouseX, mouseY, radius / 2);
+	}
+}
